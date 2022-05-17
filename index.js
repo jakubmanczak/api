4f4e434734fbaf7429e63a4258b66eb632959e87
jakubmanczak <jakub@manczak.net>
2022-05-17 15:31:29 +0200

    set res status codes, log all requests, cleanup

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
-const fs = require("fs");
 
 const app = express();
 app.use(bodyparser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-	res.send("You've reached the manczakapi.");
+	res.status(204).send("You've reached the manczakapi.");
 	console.log("-- served a / response");
 });
-
 app.get("/remoteip", (req, res) => {
-	let ip = req.socket.remoteAddress;
-	res.json(ip);
+	res.json(req.socket.remoteAddress);
 	console.log(`-- served a /remoteip response for ${ip}`);
 });
-
 app.get("/splash", (req, res) => {
 	res.json(require("./splash.json"));
+	console.log("-- served a /splash response");
 });
 app.get("/splash/generic", (req, res) => {
 	res.json(require("./splash.json").generic);
+	console.log("-- served a /splash/generic response");
 });
 app.get("/splash/videogame", (req, res) => {
 	res.json(require("./splash.json").videogame);
+	console.log("-- served a /splash/videogame response");
 });
 app.get("*", (req, res) => {
 	res.status(400).send("There isn't any response provided for such path.");
