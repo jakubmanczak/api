00e646a3c3712679b427302fec4e3fd16b079895
jakubmanczak <jakub@manczak.net>
2023-01-01 21:16:25 +0100

    add splashes & build out /splash /splashes functionality

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,44 @@ app.use(cors());
 
 app.get("/", (req, res) => {
 	res.status(200).send("200: Reached the manczakapi.");
-	console.log(`---- served a ${res.statusCode} for: ${req.path}`);
 });
-app.get("/splash", (req, res) => {
-	res
-		.status(200)
-		.send(
-			splashjson.generic[Math.floor(Math.random() * splashjson.generic.length)]
-		);
-	console.log(`---- served a ${res.statusCode} for: ${req.path}`);
+
+app.get(["/splas:plural", "/splas:plural/:type"], (req, res) => {
+	if (req.params.plural != "h" && req.params.plural != "hes") {
+		return res.status(404).send("404: Not Found."); // feign "bad endpoint"
+	}
+	let splashes = [];
+	// TODO: optimise the everloving fuck out of this.
+	if (!req.params.type) {
+		splashes.push(splashjson.generic);
+		splashes.push(splashjson.videogame);
+		splashes.push(splashjson.music);
+	}
+	if (req.params.type == "all") {
+		splashes.push(splashjson.generic);
+		splashes.push(splashjson.personal);
+		splashes.push(splashjson.videogame);
+		splashes.push(splashjson.music);
+	}
+	if (req.params.type == "generic") splashes.push(splashjson.generic);
+	if (req.params.type == "personal") splashes.push(splashjson.personal);
+	if (req.params.type == "videogame") splashes.push(splashjson.videogame);
+	if (req.params.type == "music") splashes.push(splashjson.music);
+	splashes = splashes.flat();
+	// TODO: implement splash maxlength query
+	if (splashes.length == 0) {
+		return res.status(400).send("400: Bad splash type specifier");
+	}
+	if (req.params.plural == "h") {
+		return res
+			.status(200)
+			.send(splashes[Math.floor(Math.random() * splashes.length)]);
+	}
+	return res.status(200).send(splashes);
 });
+
 app.get("*", (req, res) => {
-	res.status(404).send("404: Reached the manczakapi.");
-	console.log(`---- served a ${res.statusCode} for: ${req.path}`);
+	res.status(404).send("404: Not Found.");
 });
 
 app.listen(port, () => {
