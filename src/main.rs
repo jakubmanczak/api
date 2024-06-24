dbbfcbce3f9511b5cb24ff3d27ff8b210ad837fe
jakubmanczak <jakub@manczak.net>
2024-06-24 23:49:35 +0200

    rust cont.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
-use sqlite;
+mod handlers;
+mod setup;
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use tracing::{error, info};
 
-fn main() {
-    let connection = sqlite::open("manczak.db").unwrap();
-    let query = "SELECT * FROM splashes;";
-    
-    connection.execute(query).unwrap();
-    connection.iterate(query, |pairs| {
-        for &(name, value) in pairs.iter() {
-            println!("{} => {}", name, value.unwrap());
+#[tokio::main]
+async fn main() {
+    setup::initialise_logging();
+    setup::initialise_dotenv();
+
+    let app = Router::new().route("/", get(handlers::health));
+
+    let addr = setup::get_socket_addr();
+    let listener = match TcpListener::bind(&addr).await {
+        Ok(listener) => listener,
+        Err(e) => {
+            error!("error creating a listener: {e}");
+            panic!();
         }
-        println!("----");
-        return true;
-    }).unwrap();
+    };
+    let addr = setup::get_listener_socket_addr(&listener);
+    info!("listener socket addr is {}", addr.to_string());
+
+    match axum::serve(listener, app).await {
+        Ok(..) => info!("now serving!"),
+        Err(e) => {
+            error!("axum could not serve listener & app: {e}");
+            panic!();
+        }
+    }
 }
