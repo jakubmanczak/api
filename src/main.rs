01195d16f714c710d0abf20c76352c58af7681e2
jakubmanczak <jakub@manczak.net>
2024-07-10 23:20:53 +0200

    do cors

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod setup;
 use axum::Router;
 use routes::routes;
 use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
 use tracing::{error, info};
 
 #[tokio::main]
@@ -11,7 +12,9 @@ async fn main() {
     setup::initialise_dotenv();
     setup::initialise_sqlite_db_tables();
 
-    let app = Router::new().merge(routes());
+    let app = Router::new()
+        .merge(routes())
+        .layer(CorsLayer::new().allow_origin(Any));
 
     let addr = setup::get_socket_addr();
     let listener = match TcpListener::bind(&addr).await {
