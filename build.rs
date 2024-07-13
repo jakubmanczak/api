915ee692958e8d3c59b80371d5c441d34f0edf9b
jakubmanczak <jakub@manczak.net>
2024-07-14 00:11:10 +0200

    swap git hash get command v2

--- build.rs
+++ build.rs
@@ -2,10 +2,7 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rustc-env=GIT_HASH={}", "");
-    match Command::new("git")
-        .args(&["log", "-n", "1", "--pretty=format:%H"])
-        .output()
-    {
+    match Command::new("echo").args(&["$GITHUB_SHA"]).output() {
         Ok(output) => match String::from_utf8(output.stdout) {
             Ok(hash) => println!("cargo:rustc-env=GIT_HASH={}", hash),
             Err(_) => (),
