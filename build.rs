a4806ceebda8129f8bc65259721e1f55dc656312
jakubmanczak <jakub@manczak.net>
2024-07-14 23:45:51 +0200

    github sha pass to docker

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rustc-env=GIT_HASH={}", "");
-    match Command::new("echo").args(&["$GITHUB_SHA"]).output() {
+    match Command::new("git").args(&["rev-parse", "HEAD"]).output() {
         Ok(output) => match String::from_utf8(output.stdout) {
             Ok(hash) => println!("cargo:rustc-env=GIT_HASH={}", hash),
             Err(_) => (),
